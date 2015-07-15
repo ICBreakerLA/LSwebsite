233b445a698f1207eb1b81f7136db9c3e6321159
Lionel <lionel.sellam@gmail.com>
2015-07-15 17:32:15 +0200

    Revert the index.php to match the local version

--- src/index.php
+++ src/index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="utf-8">
     <title>Lionel Sellam | Front End Developer</title>
-    <link rel="stylesheet" href="css/normalize..min.css">
+    <link rel="stylesheet" href="css/normalize.css">
     <link href='http://fonts.googleapis.com/css?family=Oxygen:400,700' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css" />
-    <link rel="stylesheet" href="css/main.min.css">
+    <link rel="stylesheet" href="css/jquery.mCustomScrollbar.css" />
+    <link rel="stylesheet" href="css/main.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--[if lt IE 9]>
     <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -36,35 +36,35 @@
         <ul id="gallery">
             <li>
                 <a href="img/numbers-01.jpg" data-imagelightbox="d">
-                    <img src="img/numbers-01.jpg" alt="Experimentation" height="768" width="1024" />
+                    <img src="img/numbers-01.jpg" alt="Experimentation" />
                 </a>
             </li>
             <li>
                 <a href="img/numbers-02.jpg" data-imagelightbox="d">
-                    <img src="img/numbers-02.jpg" alt="Shoop da Woop" height="768" width="1024" />
+                    <img src="img/numbers-02.jpg" alt="Shoop da Woop" />
                 </a>
             </li>
             <li>
                 <a href="img/numbers-06.jpg" data-imagelightbox="d">
-                    <img src="img/numbers-06.jpg" alt="For the lulz" height="768" width="1024" >
+                    <img src="img/numbers-06.jpg" alt="For the lulz" />
 
                 </a>
             </li>
             <li>
                 <a href="img/numbers-09.jpg" data-imagelightbox="d">
-                    <img src="img/numbers-09.jpg" alt="Get to da choppa" height="768" width="1024" >
+                    <img src="img/numbers-09.jpg" alt="Get to da choppa" />
                 </a>
             </li>
             <li>
                 <a href="img/numbers-12.jpg" data-imagelightbox="d">
-                    <img src="img/numbers-12.jpg" alt="GL HF" height="768" width="1024" >
+                    <img src="img/numbers-12.jpg" alt="GL HF" />
                 </a>
             </li>
         </ul>
     </section>
     <footer>
-        <a href="http://twitter.com/icbreaker"><img src="img/twitter-wrap.png" alt="Twitter Logo" class="social-icon"></a>
-        <a href="http://facebook.com/lionel.cestlame"><img src="img/facebook-wrap.png" alt="Facebook Logo" class="social-icon"></a>
+        <a href="http://twitter.com/icbreaker"><img src="img/twitter-wrap.png" alt="Twitter Logo" class="social-icon"/></a>
+        <a href="http://facebook.com/lionel.cestlame"><img src="img/facebook-wrap.png" alt="Facebook Logo" class="social-icon"/></a>
         <p>&copy; 2015 Lionel Sellam.</p>
     </footer>
 </div>
