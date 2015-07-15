c1259ccb7d54490200c8ce4880bb1442645d5af5
Lionel <lionel.sellam@gmail.com>
2015-07-15 16:50:29 +0200

    Added minified version for Customscrollbar.css, normalize.css and main.css

--- src/index.php
+++ src/index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="utf-8">
     <title>Lionel Sellam | Front End Developer</title>
-    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/normalize..min.css">
     <link href='http://fonts.googleapis.com/css?family=Oxygen:400,700' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="css/jquery.mCustomScrollbar.css" />
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css" />
+    <link rel="stylesheet" href="css/main.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--[if lt IE 9]>
     <script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -36,28 +36,28 @@
         <ul id="gallery">
             <li>
                 <a href="img/numbers-01.jpg" data-imagelightbox="d">
-                    <img src="img/numbers-01.jpg" alt="Experimentation">
+                    <img src="img/numbers-01.jpg" alt="Experimentation" height="768" width="1024" />
                 </a>
             </li>
             <li>
                 <a href="img/numbers-02.jpg" data-imagelightbox="d">
-                    <img src="img/numbers-02.jpg" alt="Shoop da Woop">
+                    <img src="img/numbers-02.jpg" alt="Shoop da Woop" height="768" width="1024" />
                 </a>
             </li>
             <li>
                 <a href="img/numbers-06.jpg" data-imagelightbox="d">
-                    <img src="img/numbers-06.jpg" alt="For the lulz">
+                    <img src="img/numbers-06.jpg" alt="For the lulz" height="768" width="1024" >
 
                 </a>
             </li>
             <li>
                 <a href="img/numbers-09.jpg" data-imagelightbox="d">
-                    <img src="img/numbers-09.jpg" alt="Get to da choppa">
+                    <img src="img/numbers-09.jpg" alt="Get to da choppa" height="768" width="1024" >
                 </a>
             </li>
             <li>
                 <a href="img/numbers-12.jpg" data-imagelightbox="d">
-                    <img src="img/numbers-12.jpg" alt="GL HF">
+                    <img src="img/numbers-12.jpg" alt="GL HF" height="768" width="1024" >
                 </a>
             </li>
         </ul>
